0cb62aa30af83822d2fcb2cad4ff6b3dcb914600
AhmedElazony <ahmedazony2@gmail.com>
2025-05-12 20:32:37 +0300

    Clean up.

--- frontend/src/components/ui/NavLink.vue
+++ frontend/src/components/ui/NavLink.vue
@@ -2,7 +2,7 @@
   <router-link
     :to="to"
     class="hover:text-indigo-600 dark:text-white dark:hover:text-indigo-600 inline-flex text-gray-500 items-center px-1 py-2 text-sm font-medium border-b-2 border-transparent transition-colors duration-200"
-    active-class="border-indigo-600 text-indigo-600"
+    active-class="border-indigo-600 text-indigo-600 dark:border-indigo-400 dark:text-indigo-400"
   >
     <slot>{{ name }}</slot>
   </router-link>
